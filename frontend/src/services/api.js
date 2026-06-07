c708fc61e635c005b21ec33d964a458deb390c40
Shiva <gsivasurya1@gmail.com>
2026-06-07 18:53:45 +0530

    redeploy backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+export default axios.create({
+  baseURL:
+    "https://grocery-store-backend-nv11.onrender.com/api",
 });
-
-export default api;
